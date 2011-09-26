c9ab3a9a36984724d8a3047473a967a6fc5e2953
nubela <nubela@gmail.com>
2011-09-26 19:32:05 +0800

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,16 +3,12 @@ from time import sleep
 
 env.hosts = ['root@ec2-75-101-174-29.compute-1.amazonaws.com']
 
-def commit():
-    local("git add . -A")
-    local("git commit -m 'FabCommit'")
+def error():
+    with cd("/home/nubela/Workspace/radar-backend"):
+        run("cat error.log")
 
 def deploy():
-    local("git push dropbox-repo master")
-    print "Sleeping for dropbox to sync..."
-    sleep(10)
-    print "Rebasing..."
     with cd("/home/nubela/Workspace/radar-backend"):
-        run("git stash;git pull --rebase;git stash apply")
+        run("git pull --rebase")
         run("/etc/init.d/apache2 restart")
     print "Done!"
